159e67b00e2f15d61ceb4319f07eb5bd65116de0
Jake Medina <jake.medina02@gmail.com>
2023-10-12 15:34:56 -0500

    Initial Commit

---
+++ SportScoutMedinaJake/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SportScoutMedinaJake
+//
+//  Created by Jake Medina on 10/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
