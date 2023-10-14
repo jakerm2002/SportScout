b00d02f771a2a4ed2d7c4fd6b8fdd9b08d337487
Ashley Yude <ayud0371@utexas.edu>
2023-10-14 18:20:54 -0500

    Login and Signup almost done - fix sign up error message

--- SportScoutMedinaJake/LoginViewController.swift
+++ SportScoutMedinaJake/LoginViewController.swift
@@ -6,13 +6,41 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var errorMessage: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Hide password
+        passwordTextField.isSecureTextEntry = true
 
-        // Do any additional setup after loading the view.
+//        Auth.auth().addStateDidChangeListener() {
+//            (auth,user) in
+//            if user != nil {
+//                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+//                self.emailTextField.text = nil
+//                self.passwordTextField.text = nil
+//            }
+//        }
+    }
+    
+    @IBAction func loginButtonPressed(_ sender: Any) {
+        
+        Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) {
+            (authResult,error) in
+                if let error = error as NSError? {
+                    self.errorMessage.text = "Invalid email or password"
+                } else {
+                    self.errorMessage.text = ""
+                    self.performSegue(withIdentifier: "loginSegue", sender: nil)
+                }
+        }
     }
     
 
--- SportScoutMedinaJake/SignupViewController.swift
+++ SportScoutMedinaJake/SignupViewController.swift
@@ -6,14 +6,50 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class SignupViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var fullNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    @IBOutlet weak var errorMessage: UILabel!
+    
     var SignupToCustomizeProfileSegueIdentifier = "SignupToCustomizeProfileSegue"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // Hide password
+        passwordTextField.isSecureTextEntry = true
+        confirmPasswordTextField.isSecureTextEntry = true
+        
+        
+        //Check fields before segue
+        if usernameTextField == nil || fullNameTextField == nil || emailTextField == nil || passwordTextField == nil {
+            errorMessage.text = "One or more fields are missing"
+        } else if passwordTextField != confirmPasswordTextField {
+            errorMessage.text = "Passwords do not match"
+        } else {
+            errorMessage.text = ""
+            self.performSegue(withIdentifier: self.SignupToCustomizeProfileSegueIdentifier, sender: nil)
+            self.usernameTextField.text = nil
+            self.fullNameTextField.text = nil
+            self.emailTextField.text = nil
+            self.passwordTextField.text = nil
+            self.confirmPasswordTextField.text = nil
+        }
+        
+//        Auth.auth().addStateDidChangeListener() {
+//            (auth,user) in
+//            if user != nil {
+//                self.performSegue(withIdentifier: self.SignupToCustomizeProfileSegueIdentifier, sender: nil)
+//                self.emailTextField.text = nil
+//                self.passwordTextField.text = nil
+//            }
+//        }
     }
     
 
@@ -28,8 +64,21 @@ class SignupViewController: UIViewController {
     */
     
     @IBAction func signupButtonPressed(_ sender: Any) {
-        // do firebase user creation
-        self.performSegue(withIdentifier: SignupToCustomizeProfileSegueIdentifier, sender: nil)
+        // Firebase user creation
+        Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) {
+            (authResult,error) in
+                if let error = error as NSError? {
+                    self.errorMessage.text = "\(error.localizedDescription)"
+                } else {
+                    self.errorMessage.text = ""
+                    self.performSegue(withIdentifier: self.SignupToCustomizeProfileSegueIdentifier, sender: nil)
+                }
+        }
+        
+//        if passwordTextField != confirmPasswordTextField {
+//            self.errorMessage.text = "Passwords do not match"
+//        }
+        
     }
     
 
