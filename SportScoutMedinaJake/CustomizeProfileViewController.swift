39b6aca93b3afce69ccff8cdcb194ce21b7229c1
Ankit Bhattacharyya <ankitbhattacharyya@Ankits-MacBook-Air.local>
2023-10-16 00:35:57 -0500

    outlets created for customize profile

--- SportScoutMedinaJake/CustomizeProfileViewController.swift
+++ SportScoutMedinaJake/CustomizeProfileViewController.swift
@@ -7,12 +7,22 @@
 
 import UIKit
 
-class CustomizeProfileViewController: UIViewController {
+class CustomizeProfileViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var inchesField: UITextField!
+    @IBOutlet weak var feetField: UITextField!
+    @IBOutlet weak var weightField: UITextField!
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var nameField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        inchesField.delegate = self
+        feetField.delegate = self
+        weightField.delegate = self
+        usernameField.delegate = self
     }
     
 
@@ -26,4 +36,27 @@ class CustomizeProfileViewController: UIViewController {
     }
     */
 
+    @IBAction func doneButtonPressed(_ sender: Any) {
+    }
+    
+    // Called when 'return' key pressed
+    func textFieldShouldReturn(_ textField:UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    // Called when the user clicks on the view outside of the UITextField
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let allowedCharacters = CharacterSet.decimalDigits
+        let characterSet = CharacterSet(charactersIn: string)
+        if (textField == inchesField || textField == feetField) {
+            return allowedCharacters.isSuperset(of: characterSet)
+        }
+        return characterSet.isSuperset(of: characterSet)
+    }
 }
