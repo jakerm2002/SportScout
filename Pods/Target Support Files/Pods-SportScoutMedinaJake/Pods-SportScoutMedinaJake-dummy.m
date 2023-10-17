6f33b334dca1bd6442135282b06c5776dccad2f5
Jake Medina <jake.medina02@gmail.com>
2023-10-17 00:40:49 -0500

    add cocoapods, basic calendar view

---
+++ Pods/Target Support Files/Pods-SportScoutMedinaJake/Pods-SportScoutMedinaJake-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SportScoutMedinaJake : NSObject
+@end
+@implementation PodsDummy_Pods_SportScoutMedinaJake
+@end
--- SportScoutMedinaJake/SSLocationDetailsViewController.swift
+++ SportScoutMedinaJake/SSLocationDetailsViewController.swift
@@ -6,12 +6,46 @@
 //
 
 import UIKit
+import CalendarLib
 
-class SSLocationDetailsViewController: UIViewController {
+class SSLocationDetailsViewController: UIViewController, MGCDayPlannerViewDataSource {
+    func dayPlannerView(_ view: MGCDayPlannerView!, numberOfEventsOf type: MGCEventType, at date: Date!) -> Int {
+        let startDate = Date(
+            timeIntervalSince1970:
+                TimeInterval(1697727600))
+        if (date < startDate) {
+            return 1
+        }
+        return 0
+    }
+    
+    func dayPlannerView(_ view: MGCDayPlannerView!, viewForEventOf type: MGCEventType, at index: UInt, date: Date!) -> MGCEventView! {
+        let event = MGCStandardEventView()
+        event.title = "Sarah's Team"
+        event.subtitle = "Volleyball"
+        return event
+    }
+    
+    func dayPlannerView(_ view: MGCDayPlannerView!, dateRangeForEventOf type: MGCEventType, at index: UInt, date: Date!) -> MGCDateRange! {
+        return MGCDateRange(
+            start:
+                Date(
+                    timeIntervalSince1970:
+                        TimeInterval(1697727600)),
+            end:
+                Date(
+                    timeIntervalSince1970:
+                        TimeInterval(1697734800))
+        )
+    }
+    
 
     @IBOutlet weak var locationNameTextLabel: UILabel!
     @IBOutlet weak var locationAddrTextLabel: UILabel!
     @IBOutlet weak var locationCityStateZipTextLabel: UILabel!
+    
+    @IBOutlet weak var calendarView: MGCDayPlannerView!
+    
     
     var LocationObject:Location!
     var documentID = "" // will be set from home VC
@@ -22,6 +56,7 @@ class SSLocationDetailsViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         fetchData()
+        calendarView.dataSource = self
     }
     
     func fetchData() {
