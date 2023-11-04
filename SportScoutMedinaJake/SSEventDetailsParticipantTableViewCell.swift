900a0ec23ead2f165716057e37855068d27f745a
Ashley Yude <ayud0371@utexas.edu>
2023-11-03 19:39:43 -0500

    created EventDetailsParticipantTableVideCell & edited EventDetails VC

---
+++ SportScoutMedinaJake/SSEventDetailsParticipantTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SSEventDetailsParticipantTableViewCell.swift
+//  SportScoutMedinaJake
+//
+//  Created by Ashley Nicole Yude on 11/3/23.
+//
+
+import UIKit
+
+class SSEventDetailsParticipantTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
