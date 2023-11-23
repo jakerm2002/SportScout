9b48bfecfe245935204fd9d459577b9ea503e314
Jake Medina <jake.medina02@gmail.com>
2023-11-22 23:47:24 -0600

    move HomeTblViewCell to separate file, fix constraints for home screen cells

--- SportScoutMedinaJake/HomeViewController.swift
+++ SportScoutMedinaJake/HomeViewController.swift
@@ -15,33 +15,6 @@ import FirebaseStorage
 let db = Firestore.firestore()
 let storage = Storage.storage()
 
-// TODO: Move to its own file
-class SSHomeTableViewCell: UITableViewCell {
-    @IBOutlet weak var locationTitleTextLabel: UILabel!
-    @IBOutlet weak var locationAddressTextLabel: UILabel!
-    @IBOutlet weak var locationImageView: UIImageView!
-    
-    // Rounded corners and drop shadow for table view cells
-    // https://stackoverflow.com/questions/37645408/uitableviewcell-rounded-corners-and-shadow
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        let bottomSpace: CGFloat = 10.0
-        self.contentView.frame = self.contentView.frame.inset(by: UIEdgeInsets(top: 0, left: 0, bottom: bottomSpace, right: 0))
-        
-        // add shadow on cell
-        self.backgroundColor = .clear
-        self.layer.masksToBounds = false
-        self.layer.shadowOpacity = 0.23
-        self.layer.shadowRadius = 4
-        self.layer.shadowOffset = CGSize(width: 0, height: 0)
-        self.layer.shadowColor = UIColor.black.cgColor
-        
-        // add corner radius on `contentView`
-        self.contentView.backgroundColor = .white
-        self.contentView.layer.cornerRadius = 8
-    }
-}
-
 class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var homeTableView: UITableView!
---
+++ SportScoutMedinaJake/SSHomeTableViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  SSHomeTableViewCell.swift
+//  SportScoutMedinaJake
+//
+//  Created by Jake Medina on 11/21/23.
+//
+
+import UIKit
+
+class SSHomeTableViewCell: UITableViewCell {
+    @IBOutlet weak var locationTitleTextLabel: UILabel!
+    @IBOutlet weak var locationAddressTextLabel: UILabel!
+    @IBOutlet weak var locationImageView: UIImageView!
+    
+    // Rounded corners and drop shadow for table view cells
+    // https://stackoverflow.com/questions/37645408/uitableviewcell-rounded-corners-and-shadow
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        let bottomSpace: CGFloat = 10.0
+        self.contentView.frame = self.contentView.frame.inset(by: UIEdgeInsets(top: 0, left: 0, bottom: bottomSpace, right: 0))
+        
+        // add shadow on cell
+        self.backgroundColor = .clear
+        self.layer.masksToBounds = false
+        self.layer.shadowOpacity = 0.23
+        self.layer.shadowRadius = 4
+        self.layer.shadowOffset = CGSize(width: 0, height: 0)
+        self.layer.shadowColor = UIColor.black.cgColor
+        
+        // add corner radius on `contentView`
+        self.contentView.backgroundColor = .white
+        self.contentView.layer.cornerRadius = 8
+    }
+}
