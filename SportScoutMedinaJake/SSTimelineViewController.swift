a31581e3bd0505f949e6fca65998505cf46b7623
Jake Medina <jake.medina02@gmail.com>
2023-11-24 15:34:26 -0600

    some UI for timeline cells

--- SportScoutMedinaJake/SSTimelineViewController.swift
+++ SportScoutMedinaJake/SSTimelineViewController.swift
@@ -7,17 +7,23 @@
 
 import UIKit
 
-class SSTimelineViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+class SSTimelineViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     @IBOutlet weak var collectionView: UICollectionView!
     
     let timelineCollectionViewCellIdentifier = "TimelineCollectionViewCellIdentifier"
+    
+    var rightNavBarSegment = UIBarButtonItem(systemItem: .add)
+    var searchBar = UISearchBar()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         collectionView.delegate = self
         collectionView.dataSource = self
+        
+        navigationItem.rightBarButtonItem = rightNavBarSegment
+        navigationItem.titleView = searchBar
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -30,15 +36,18 @@ class SSTimelineViewController: UIViewController, UICollectionViewDelegate, UICo
         return cell
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLayoutSubviews() {
+        let layout = UICollectionViewFlowLayout()
+        let containerWidth = collectionView.bounds.width
+        let numColumns = 1.0
+        let cellSize = (containerWidth - 40) / numColumns
+        
+        layout.itemSize = CGSize(width: cellSize, height: cellSize)
+        layout.minimumLineSpacing = 5
+        layout.minimumInteritemSpacing = 5
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        
+        collectionView.collectionViewLayout = layout
     }
-    */
 
 }
